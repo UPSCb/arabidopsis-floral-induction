24237c3ea80d8ff1d5ae5143314d9f227ccdf1b8
Nicolas <nicolas.delhomme@umu.se>
2020-07-10 14:44:01 +0200

    Prepared to run the blast search against the NCBI

--- pipeline/submitTBlastN.sh
+++ pipeline/submitTBlastN.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -l
 in=../data/raw/peptide.faa
-db=../reference/indices/BLAST+/TAIR
+#db=../reference/indices/BLAST+/TAIR
+db=../NCBI/nt
 out=../data/BLAST+
 
 if [ ! -d $out ]; then
@@ -9,4 +10,4 @@ fi
 
 sbatch -p nolimit -t unlimited -n 12 -A u2019022 \
 -o $out/peptide-TAIR10.out -e $out/peptide-TAIR10.err \
-../UPSCb-common/pipeline/runBlastPlus.sh -p 12 -e 10 tblastn $in $db $out  
+../UPSCb-common/pipeline/runBlastPlus.sh -p 12 -e 10 tblastn $in $db $out
